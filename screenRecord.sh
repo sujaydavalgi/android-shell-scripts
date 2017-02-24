8e5b2eba89fea8d74493698f29330ade9d5cd0fb
Sujay Davalgi <sujay@davalgi.com>
2017-02-23 21:45:10 -0800

    remove root access check for screenrecord
    remove the root access check for adb screenrecord command

--- screenRecord.sh
+++ screenRecord.sh
@@ -25,17 +25,17 @@ getDeviceChoice
 displaySelectedDevice $deviceSerial
 
 if [ $( checkAdbDevice $deviceSerial ) == "true" ]; then
-	if [ "$( checkDevKeyDevice $deviceSerial )" == "true" ]; then
+	#if [ "$( checkDevKeyDevice $deviceSerial )" == "true" ]; then
 		fileName=`echo $( getFormatedFileName $deviceSerial ${fileName} )`
 
 		echo -e -n " Your video will be saved in ${RecordFolder} as : ${fileName}.mp4\n\n"
 
-		adb -s $deviceSerial wait-for-device root
-		sleep 1s
+		#adb -s $deviceSerial wait-for-device root
+		#sleep 1s
 		recordDeviceVideo $deviceSerial ${RecordFolder} ${fileName}
-	else
-		echo -e -n " Device doesnot support root access\n"
-	fi
+	#else
+	#	echo -e -n " Device doesnot support root access\n"
+	#fi
 else
 	echo -e -n " Device is not in adb mode\n"
 fi

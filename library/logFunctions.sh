b0670aa49f809954515c2ae2e3cda63d2bd9abaa
Sujay Davalgi <sujay@davalgi.com>
2017-03-03 00:02:31 -0800

    include the file extensions as variables

--- library/logFunctions.sh
+++ library/logFunctions.sh
@@ -11,12 +11,36 @@
 #===================================================================================================
 
 # Pre-M (<6.0) txt, M (=6.0) txt, Post-M (>=7.0) zip
-bugreportBuildVersionCutoff="7.0"
+newBugreportSdkVersion="7.0"
+
+#===================================================================================================
+
+#----- file name format
+function getFormatedFileName() {
+#$1 is device serial number
+#$2 is filename
+#$return - 
+	if [ $# -lt 2 ]; then
+		writeToLogsFile "@@ No 2 argument passed to ${FUNCNAME[0]}() in ${BASH_SOURCE} called from $( basename ${0} )"
+		exit 1
+	else
+
+        if [ "$( isGoogleDevice $1 )" == "true" ]; then
+            #echo -e -n "$( getDeviceName $1)_$( getDeviceBuild $1)_${2}_${nowTime}"
+            echo -e -n "$( getDeviceName $1)_${2}_${nowTime}"
+        else
+            echo -e -n "${2}_${nowTime}"
+        fi
+	fi
+}
+
+#===================================================================================================
 
 function takeBugreport() {
 #$1 is device serial number
 #$2 is filename
 #$return - take bugreport
+#https://android.googlesource.com/platform/frameworks/native/+/master/cmds/dumpstate/bugreport-format.md
 	if [ $# -lt 2 ]; then
 		writeToLogsFile "@@ No 2 argument passed to ${FUNCNAME[0]}() in ${BASH_SOURCE} called from $( basename ${0} )"
 		exit 1
@@ -24,13 +48,13 @@ function takeBugreport() {
 		#TODO check the device version and then choose which file extension type to use
 		# Pre-M (<6.0) txt, M (=6.0) txt, Post-M (>=7.0) zip
 
-		local compareBuildVersionStatus=$( compareDeviceBuildVersion ${1} ${bugreportBuildVersionCutoff} )
+		local compareBuildVersionStatus=$( compareDeviceBuildVersion ${1} ${newBugreportSdkVersion} )
 
 		if [[ ${compareBuildVersionStatus} == "same" || ${compareBuildVersionStatus} == "greater" ]]; then
-			adb -s "$1" wait-for-device bugreport `echo ${myLogs}/`${2}.zip
+			adb -s "$1" wait-for-device bugreport `echo ${myLogs}/`${2}.${bugreport2Extension}
 			#echo -e -n " zip"
 		else
-			adb -s "$1" wait-for-device bugreport > `echo ${myLogs}/`${2}.txt
+			adb -s "$1" wait-for-device bugreport > `echo ${myLogs}/`${2}.${bugreportExtension}
 			#echo -e -n " txt"
 		fi
 
@@ -46,12 +70,12 @@ function getBugreport() {
 	else
 		echo -e -n "\n Taking Bugreport... "
 
-		local compareBuildVersionStatus=$( compareDeviceBuildVersion ${1} ${bugreportBuildVersionCutoff} )
+		local compareBuildVersionStatus=$( compareDeviceBuildVersion ${1} ${newBugreportSdkVersion} )
 			
 		if [[ ${compareBuildVersionStatus} == "same" || ${compareBuildVersionStatus} == "greater" ]]; then
-			echo -e -n " ${2}.zip\n\n"
+			echo -e -n " ${2}.${bugreport2Extension}\n\n"
 		else
-			echo -e -n " ${2}.txt\n\n"
+			echo -e -n " ${2}.${bugreportExtension}\n\n"
 		fi
 
 		takeBugreport ${1} ${2}
@@ -68,7 +92,7 @@ function saveLogcat() {
 		writeToLogsFile "@@ No 2 argument passed to ${FUNCNAME[0]}() in ${BASH_SOURCE} called from $( basename ${0} )"
 		exit 1
 	else
-		adb -s "$1" logcat -v threadtime | tee `echo ${myLogs}/`${2}-logcat.txt
+		adb -s "$1" logcat -v threadtime | tee `echo ${myLogs}/`${2}-logcat.${logcatExtension}
 	fi
 }
 
@@ -96,15 +120,23 @@ function takeScreenshot() {
 		#echo -e -n " You are using ${myOS} machine\n"
 		
 		#TODO this logic is not always working. The screencap command is behaving differently on different OS, based on the device version
-		#adb -s "$1" wait-for-device shell screencap -p > `echo ${myLogs}/`${2}.png
-		if [ ${myOS} == "linux" ]; then
-		      adb -s "$1" wait-for-device shell screencap -p | sed 's/\r$//' > `echo ${myLogs}/`${2}.png
-		elif [ ${myOS} == "mac" ]; then
-		      adb -s "$1" wait-for-device shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g' > `echo ${myLogs}/`${2}.png
-		      #adb -s "$1" wait-for-device shell screencap -p | perl -pe 'BEGIN { $/="\cM\cJ"; $\="\cJ"; } chomp;' > `echo ${myLogs}/`${2}.png
-		else
-			adb -s "$1" wait-for-device shell screencap > `echo ${myLogs}/`${2}.png
-		fi
+		#adb -s "$1" wait-for-device shell screencap -p > `echo ${myLogs}/`${2}.${screenshotExtension}
+
+		#if [ ${myOS} == "linux" ]; then
+		#      adb -s "$1" wait-for-device shell screencap -p | sed 's/\r$//' > `echo ${myLogs}/`${2}.${screenshotExtension}
+		#elif [ ${myOS} == "mac" ]; then
+		#      adb -s "$1" wait-for-device shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g' > `echo ${myLogs}/`${2}.${screenshotExtension}
+		#      #adb -s "$1" wait-for-device shell screencap -p | perl -pe 'BEGIN { $/="\cM\cJ"; $\="\cJ"; } chomp;' > `echo ${myLogs}/`${2}.${screenshotExtension}
+		#else
+		#	adb -s "$1" wait-for-device shell screencap -p > `echo ${myLogs}/`${2}.${screenshotExtension}
+		#fi
+
+		#Trying to take the screenshot in the device sdcard itself
+		#adb -s "$1" wait-for-device shell screencap ${deviceScreenshotFolder}/${2}.${screenshotExtension}
+
+		#Trying to use exec-out instead of shell
+		#IMP: check how we use exec-out instead of shell
+		adb -s "$1" wait-for-device exec-out screencap -p > `echo ${myLogs}/`${2}.${screenshotExtension}
 	fi
 }
 
@@ -117,8 +149,12 @@ function getScreenshot() {
 		exit 1
 	else
 		echo -e -n "\n Taking Screenshot... "
-		echo -e -n " ${2}.png"
+		echo -e -n " ${2}.${screenshotExtension}"
 		takeScreenshot ${1} ${2}
+		
+		# use this if we are saving the screenshot file in the sdcard itself (in function takeScreenshot)
+		#pullDeviceSingleFileFrmFldr ${1} ${deviceScreenshotFolder} "${2}.${screenshotExtension}"
+		
 		echo -e -n "\n ...Done\n"
 	fi
 }
@@ -132,28 +168,8 @@ function recordDeviceVideo() {
 		writeToLogsFile "@@ No 3 argument passed to ${FUNCNAME[0]}() in ${BASH_SOURCE} called from $( basename ${0} )"
 		exit 1
 	else
-		adb -s "$1" wait-for-device shell "screenrecord --verbose --bit-rate 4000000 ${2}/${3}.mp4" # <- 4 Mbps
-		#adb -s $1 wait-for-device shell "screenrecord --verbose --bit-rate 8000000 $2/${3}.mp4" # <- 8 Mbps
-	fi
-}
-#===================================================================================================
-
-#----- file name format
-function getFormatedFileName() {
-#$1 is device serial number
-#$2 is filename
-#$return - 
-	if [ $# -lt 2 ]; then
-		writeToLogsFile "@@ No 2 argument passed to ${FUNCNAME[0]}() in ${BASH_SOURCE} called from $( basename ${0} )"
-		exit 1
-	else
-
-        if [ "$( checkGoogleDevice $1 )" == "true" ]; then
-            #echo -e -n "$( getDeviceName $1)_$( getDeviceBuild $1)_${2}_${nowTime}"
-            echo -e -n "$( getDeviceName $1)_${2}_${nowTime}"
-        else
-            echo -e -n "${2}_${nowTime}"
-        fi
+		adb -s "$1" wait-for-device shell "screenrecord --verbose --bit-rate 4000000 ${2}/${3}.${screenrecordExtension}" # <- 4 Mbps
+		#adb -s $1 wait-for-device shell "screenrecord --verbose --bit-rate 8000000 $2/${3}.${screenrecordExtension}" # <- 8 Mbps
 	fi
 }
 

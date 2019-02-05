742d2fe48a21fb279acebd284c2d565b8c738862
Sujay Davalgi <sujay_ds@hotmail.com>
2019-02-05 13:35:13 -0800

    New script to get the device properties

---
+++ getProp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Created by: Sujay Davalgi
+#
+# Displays the logcat in the terminal
+# If argument was provided while initiating the command, it will also save the output in a specified file
+#
+# Usage: ./myLC.sh [<filename>]
+# Arguments (Optional):
+#	$1 - File name to be saved as.
+#		No necessary to give the extension, by default it will save as ".txt"
+#		If, filename is not provided, it will only display the logcat and not save the output to a file
+
+. ./library/mainFunctions.sh
+. ./library/textFormatting.sh
+. ./library/deviceOperations.sh
+. ./library/logFunctions.sh
+
+getDeviceChoice
+displaySelectedDevice $deviceSerial
+
+if [ $( isAdbDevice $deviceSerial ) == "true" ]; then
+	adb -s $deviceSerial shell getprop | grep -i "$1"
+else
+	echo " Device is not in 'adb' mode"
+fi

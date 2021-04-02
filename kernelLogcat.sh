21b3a2262c92711d4a44d444f494c2290f690029
Sujay Davalgi <sujay_ds@hotmail.com>
2021-04-02 15:28:36 -0700

    Script to Get the Kernel logs

---
+++ kernelLogcat.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Created by: Sujay Davalgi
+#
+# Displays the kernel logcat in the terminal
+# If argument was provided while initiating the command, it will also save the output in a specified file
+#
+# Usage: ./kernelLogcat.sh [<filename>]
+# Arguments (Optional):
+#	$1 - File name to be saved as.
+#		No necessary to give the extension, by default it will save as ".txt"
+#		If, filename is not provided, it will only display the logcat and not save the output to a file
+
+. ./library/mainFunctions.sh
+. ./library/textFormatting.sh
+. ./library/deviceOperations.sh
+. ./library/logFunctions.sh
+
+getDeviceChoice
+displaySelectedDevice $deviceSerial
+
+if [ $( isAdbDevice $deviceSerial ) == "true" ]; then
+	if [ $# -lt 1 ]; then
+		adb -s $deviceSerial logcat -v threadtime
+	else
+		fileName="`echo $( getFormatedFileName $deviceSerial "$1" )`"
+		echo -e " Your file will be saved in folder : ${myLogs} as : ${fileName}-logcat.txt \n"
+		saveKernelLogcat $deviceSerial ${fileName}
+	fi
+else
+	echo " Device is not in 'adb' mode"
+fi
